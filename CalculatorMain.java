85529eadc3f9f4131aa31cc1efefe54ca1efd109
AkashSinghWissen <akash.singh1@wissen.com>
2025-02-13 15:09:42 +0530

    Add files via upload

---
+++ CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        System.out.println("Program has started");
+ 
+        Calculator c1 = new Calculator();
+        c1.add(10, 20);
+        c1.sub(10, 20);
+        c1.mul(10, 20);
+    }
+}
